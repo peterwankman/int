992152af205f52dbd1a6e484ac3a5de3baf402dd
Martin Wolters <sirdzstic@gmail.com>
2012-06-19 20:13:24 +0200

    WTFPL license

--- src/eval.c
+++ src/eval.c
@@ -1,3 +1,14 @@
+/*
+ * int -- A script interpreter for boskop-ng
+ * (C) 2012 Martin Wolters
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+
 #include <ctype.h>
 #include <limits.h>
 #include <stdio.h>
